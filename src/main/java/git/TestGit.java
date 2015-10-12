867aa556bd3cb96f172945c55292ef677eab0e7c
meitao <869363941@qq.com>
2015-10-12 21:56:03 +0800

    22

--- src/main/java/git/TestGit.java
+++ src/main/java/git/TestGit.java
@@ -3,8 +3,6 @@ package git;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 } 
--- src/test/java/junit/Test.java
+++ src/test/java/junit/Test.java
@@ -1,5 +1,5 @@
 package junit;
 
 public class Test {
-
+  
 }
